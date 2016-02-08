1e6489246e21b1ed810d04d488333b01a30aeb2f
Ryutaro YOSHIBA <ryuzee@gmail.com>
2016-02-08 20:03:10 +0900

    fix samples 1) fix Berksfile to handle with changing endpoint 2) install Java8 to run latest version of Jenkins 3) add procedure to change default ruby version

--- site-cookbooks/capistrano/recipes/default.rb
+++ site-cookbooks/capistrano/recipes/default.rb
@@ -2,6 +2,15 @@ package "ruby2.0" do
   action :install
 end
 
+# P136 ruby2.0に切り替えるために以下の処理を実行
+bash 'symlink' do
+  code <<-EOH
+    for prog in /usr/bin/{erb,gem,testrb,irb,rake,ruby,ri,rdoc}; do sudo ln -sf ${prog}2.0 $prog; done
+    sudo gem update --system
+    sudo gem pristine --all
+  EOH
+end
+
 gem_package "capistrano" do
   action :install
 end
--- site-cookbooks/jenkins/recipes/default.rb
+++ site-cookbooks/jenkins/recipes/default.rb
@@ -7,6 +7,31 @@ apt_repository "jenkins" do
   action :add
 end
 
+# JenkinsがJDK8に依存するように変更になったので古いバージョンを除去
+%w{openjdk-6-jre openjdk-6-jdk}.each do |package_name|
+  package package_name do
+    action :remove
+  end
+end
+
+# add-apt-repositoryを利用できるようにする
+package 'software-properties-common' do
+  action :install
+end
+
+# open-jdk8のレポジトリを追加
+bash 'add-apt-repository' do
+  code <<-EOH
+    sudo add-apt-repository ppa:openjdk-r/ppa
+    sudo apt-get update
+  EOH
+end
+
+# open-jdk8をインストール
+package "openjdk-8-jdk" do
+  action :install
+end
+
 package "jenkins" do
   action :install
 end
